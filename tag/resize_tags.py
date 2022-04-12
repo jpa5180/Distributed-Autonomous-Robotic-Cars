ce06ec8877a8edee826fab39bb852d55d3ffb3b8
dstdaskjl <55066249+dstdaskjl@users.noreply.github.com>
2022-04-12 10:51:08 -0400

    Deleted unused files

--- tag/resize_tags.py
+++
@@ -1,8 +0,0 @@
-import os
-
-directory = 'tags/'
-for filename in os.listdir(directory):
-    orig_path = os.path.join(directory, filename)
-    new_path = os.path.join('resized_tags', filename)
-    command = 'convert ' + orig_path + ' -scale 2000% ' + new_path
-    os.system(command)
